ff73a3c05810e3dc14bd1e814877e3226fd1f983
SahilMehdiyev <branchsahil@gmail.com>
2024-10-03 23:29:57 +0400

    InsertData Validation

--- dataentry/management/commands/insertdata.py
+++ dataentry/management/commands/insertdata.py
@@ -7,12 +7,19 @@ class Command(BaseCommand):
     def handle(self,*args, **kwargs):
         
         dataset = [
-            {'roll_no': 1002, 'name': 'Test2', 'age': '22'},
-            {'roll_no': 1003, 'name': 'Test3', 'age': '23'},
-            {'roll_no': 1004, 'name': 'Test4', 'age': '24'},
+            {'roll_no': 1002, 'name': 'Test2', 'age': '24'},
+            {'roll_no': 1003, 'name': 'Test3', 'age': '25'},
+            {'roll_no': 1004, 'name': 'Test4', 'age': '26'},
         ]
         
         for data in dataset:
-            # print(data['name'])
-            Student.objects.create(roll_no=data['roll_no'], name= data['name'], age=data['age'])
+            roll_no = data['roll_no']
+            existing_record = Student.objects.filter(roll_no=roll_no).exists()
+            
+            if not existing_record:
+                Student.objects.create(roll_no=data['roll_no'], name= data['name'], age=data['age'])
+                
+            else:
+                self.stdout.write(self.style.WARNING(f'Student with roll no {roll_no} already exists! '))
+                    
         self.stdout.write(self.style.SUCCESS('Data inserted successfully!'))

ddd607075e649ba5cd17b47ae8e2a2fb7c1a03d6
SahilMehdiyev <branchsahil@gmail.com>
2024-10-04 23:05:49 +0400

    import data to any model

--- dataentry/management/commands/importdata.py
+++ dataentry/management/commands/importdata.py
@@ -1,6 +1,7 @@
 import csv
-from django.core.management.base import BaseCommand
-from dataentry.models import Student
+from django.core.management.base import BaseCommand,CommandError
+# from dataentry.models import Student
+from django.apps import apps
 
 class Command(BaseCommand):
     help = 'Import data from CVS file'
@@ -8,15 +9,31 @@ class Command(BaseCommand):
     
     def add_arguments(self,parser):
         parser.add_argument('file_path',type=str, help = 'Path to the CSV file')
+        parser.add_argument('model_name',type=str,help='Name of the model')
+        
     
     
     def handle(self,*args,**kwargs):
         
         file_path = kwargs['file_path']
+        model_name = kwargs['model_name'].capitalize()
+        
+        model = None
+        
+        for app_config in apps.get_app_configs():
+            try:
+                model = apps.get_model(app_config.label, model_name)
+                break
+            except LookupError:
+                continue
+        
+        if not model:
+            raise CommandError(f'Model "{model_name}" not found in any app!') 
+        
         with open(file_path, 'r') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                Student.objects.create(**row)
+                model.objects.create(**row)
                 
         self.stdout.write(self.style.SUCCESS('Data inserted succesfully'))
         

45128ba863748dffc1845fbc272f0ca1df1134f8
SahilMehdiyev <branchsahil@gmail.com>
2024-10-05 00:07:09 +0400

    exported data all method

--- dataentry/management/commands/exportdata.py
+++ dataentry/management/commands/exportdata.py
@@ -1,35 +1,53 @@
 from django.core.management.base import BaseCommand
 import csv
-from dataentry.models import Student
+# from dataentry.models import Student
+from django.apps import apps
 import datetime
 
 # propsed command -> make exportdata
 
 
 class Command(BaseCommand):
-    help = 'Export data from Student model to o CSV file'
+    help = 'Export data from the database to o CSV file'
+    
+    def add_arguments(self,parser):
+        parser.add_argument('model_name',type=str, help='Model name')
     
     def handle(self,*args,**kwargs):
+        model_name = kwargs['model_name'].capitalize()
+        
+        model = None
+        for app_config in apps.get_app_configs():
+            try:
+                model = apps.get_model(app_config.label, model_name)
+                break
+            except LookupError:
+                pass
+        if not model:
+            self.stderr.write(f'Model {model_name} count not found')
+            return 
+        
+        
         # fetch the data from the database 
-        students = Student.objects.all()
+        data = model.objects.all()
 
         # genearte the timestamo of current date and time
         timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 
 
         # define the csv file name/path
-        file_path = f'exported_students_data_{timestamp}.csv'       
+        file_path = f'exported_{model_name}_data_{timestamp}.csv'       
         
         # open the csv file and write the data
         with open(file_path, 'w', newline='') as file:
             writer = csv.writer(file)
             
             # write the csv header
-            writer.writerow(['Roll No', 'Name', 'Age'])
+            writer.writerow([field.name for field in model._meta.fields])
             
             # write data rows
-            for student in students:
-                writer.writerow([student.roll_no, student.name, student.age])
+            for dt in data:
+                writer.writerow([getattr(dt,field.name) for field in model._meta.fields])
         
         self.stdout.write(self.style.SUCCESS('Data exported successfully!'))
         
